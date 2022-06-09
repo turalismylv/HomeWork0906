57e2a52c7193885ec53ba2e4bb36b3fb8e58f56b
turalismylv <turalhi@code.edu.az>
2022-06-09 15:55:49 +0400

    HomeWork0906

---
+++ ConsoleApp1/ConsoleApp1/IAbles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface IAbles
+    {
+        public abstract void Add(object value);
+    }
+    interface IAddRangeable
+    {
+
+        public abstract void AddRange(ICollection c);
+    }
+    interface IRemoveable 
+    {
+        public abstract void Removeab(object? obj);
+
+    }
+    interface IRemoveAtble
+    {
+        public abstract void RemoveAt(int index);
+
+
+    }
+    interface ITrimable
+    {
+        public abstract void Trim();
+
+    }
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Custom Arraylist classi yazin, Capacity, Count proplari olsun, Capacity qebul eden constructoru olsun
+            //Add metodu, Remove metodu, RemoveAt metodu, TrimToSize metodu, AddRange metodu olsun
+
+            ArrayList arr = new ArrayList(3);
+            arr.Add(1);
+            arr.Add(2);
+            arr.Add(3);
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    class ArrayList : IAddRangeable, IRemoveable, IRemoveAtble, ITrimable, IAbles, IEnumerable
+    {
+
+        public int Capacity { get; set; }
+        public int Count { get; set; }
+
+        public object[] arr;
+
+        public ArrayList(int capacity)
+        {
+            arr = new object[capacity];
+
+        }
+
+        public void RemoveAt(int index)
+        {
+
+            for (int i = index; index < arr.Length - 1; index++)
+            {
+                arr[index] = arr[index + 1];
+            }
+            Array.Resize(ref arr, arr.Length - 1);
+
+        }
+
+        public void Removeab(object? obj)
+        {
+
+            foreach (var item in arr)
+            {
+                if (obj == item)
+                {
+                    for (int i = 0; i < arr.Length - 1; i++)
+                    {
+                        arr[i] = arr[i + 1];
+                    }
+                }
+            }
+            Array.Resize(ref arr, arr.Length - 1);
+
+        }
+        public void Add(object value)
+        {
+
+
+
+        }
+
+        public void Trim()
+        {
+            
+
+        }
+
+        public void AddRange(ICollection c)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < c.Count; j++)
+                {
+                    Array.Resize(ref arr, j);
+                    c.CopyTo(arr, i);
+                }
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return arr.GetEnumerator();
+        }
+
+        
+
+        
+
+        
+       
+    }
+}
+
+
